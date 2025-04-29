facb9ff4631d1d3133183d7bd903f0fd17f31c8a
edudscrc <eduardo.sa.coelho@gmail.com>
2025-04-29 13:41:34 -0300

    mapping points to grid (large points values to limited pixel grid

--- src/main.cpp
+++ src/main.cpp
@@ -12,71 +12,78 @@
 #include <vector>
 #include <iostream>
 
+float mapPointToGridY(float value, float yLimMin, float yLimMax, float yGridMin, float yGridMax, float windowHeight) {
+    return windowHeight - ((value - yLimMin) / (yLimMax - yLimMin) * (yGridMax - yGridMin) + yGridMin);
+}
+
+float mapPointToGridX(float value, float xLimMin, float xLimMax, float xGridMin, float xGridMax) {
+    return (value - xLimMin) / (xLimMax - xLimMin) * (xGridMax - xGridMin) + xGridMin;
+}
+
 int main()
 {
-    sf::RenderWindow window {sf::VideoMode{{500u, 500u}}, "Real-Time Plot"};
+    constexpr uint16_t windowWidth { 500u };
+    constexpr uint16_t windowHeight { 500u };
+
+    sf::RenderWindow window {sf::VideoMode{{windowWidth, windowHeight}}, "Real-Time Plot"};
 
     window.setFramerateLimit(100);
 
+    constexpr float xGridMin { 50.f };
+    constexpr float xGridMax { 450.f };
+    constexpr float yGridMin { 50.f };
+    constexpr float yGridMax { 450.f };
+
     sf::VertexArray xAxis {sf::PrimitiveType::Lines, 2};
-    xAxis[0].position = sf::Vector2f{50.f, 450.f};
-    xAxis[1].position = sf::Vector2f{450.f, 450.f};
+    xAxis[0].position = sf::Vector2f{xGridMin, yGridMax};
+    xAxis[1].position = sf::Vector2f{xGridMax, yGridMax};
     xAxis[0].color = sf::Color::Blue;
-    xAxis[1].color = sf::Color::Red;
+    xAxis[1].color = sf::Color::Blue;
 
     sf::VertexArray yAxis {sf::PrimitiveType::Lines, 2};
-    yAxis[0].position = sf::Vector2f{50.f, 50.f};
-    yAxis[1].position = sf::Vector2f{50.f, 450.f};
+    yAxis[0].position = sf::Vector2f{xGridMin, yGridMin};
+    yAxis[1].position = sf::Vector2f{xGridMin, yGridMax};
     yAxis[0].color = sf::Color::Blue;
-    yAxis[1].color = sf::Color::Red;
+    yAxis[1].color = sf::Color::Blue;
 
     sf::VertexArray topLimit {sf::PrimitiveType::Lines, 2};
-    topLimit[0].position = sf::Vector2f{50.f, 50.f};
-    topLimit[1].position = sf::Vector2f{450.f, 50.f};
+    topLimit[0].position = sf::Vector2f{xGridMin, yGridMin};
+    topLimit[1].position = sf::Vector2f{xGridMax, yGridMin};
     topLimit[0].color = sf::Color::Green;
     topLimit[1].color = sf::Color::Green;
 
-    sf::VertexArray bottomLimit {sf::PrimitiveType::Lines, 2};
-    bottomLimit[0].position = sf::Vector2f{450.f, 50.f};
-    bottomLimit[1].position = sf::Vector2f{450.f, 450.f};
-    bottomLimit[0].color = sf::Color::Green;
-    bottomLimit[1].color = sf::Color::Green;
+    sf::VertexArray rightLimit {sf::PrimitiveType::Lines, 2};
+    rightLimit[0].position = sf::Vector2f{xGridMax, yGridMin};
+    rightLimit[1].position = sf::Vector2f{xGridMax, yGridMax};
+    rightLimit[0].color = sf::Color::Green;
+    rightLimit[1].color = sf::Color::Green;
 
-    sf::VertexArray points {sf::PrimitiveType::Points, 10};
-    points[0].position = sf::Vector2f{200.f, 250.f};
-    points[1].position = sf::Vector2f{210.f, 250.f};
-    points[2].position = sf::Vector2f{220.f, 250.f};
-    points[3].position = sf::Vector2f{230.f, 250.f};
-    points[4].position = sf::Vector2f{240.f, 250.f};
-    points[5].position = sf::Vector2f{250.f, 250.f};
-    points[6].position = sf::Vector2f{260.f, 250.f};
-    points[7].position = sf::Vector2f{270.f, 250.f};
-    points[8].position = sf::Vector2f{280.f, 250.f};
-    points[9].position = sf::Vector2f{290.f, 250.f};
-
-    sf::Transform t {};
-
-
-
-    t.scale({0.5f, 0.5f});
-
-    std::vector<sf::VertexArray> yFixedValues {};
-
-    float yAxisHeight { std::abs(yAxis[0].position.y - yAxis[1].position.y) };
-    int32_t yAxisSections = static_cast<int32_t>(yAxisHeight / 10.f);
-
-    std::cout << yAxisSections << "\n";
-
-    float yAxisFirstPosition { yAxis[0].position.y };
-    
-    for (size_t i {1}; i < yAxisSections - 1; ++i) {
-        sf::VertexArray newVertexArray {sf::PrimitiveType::Lines, 2};
-
-        newVertexArray[0].position = sf::Vector2f{45.f, yAxisFirstPosition + (yAxisHeight / yAxisSections) * i};
-        newVertexArray[1].position = sf::Vector2f{55.f, yAxisFirstPosition + (yAxisHeight / yAxisSections) * i};
-
-        yFixedValues.push_back(newVertexArray);
+    int numPoints = 4500;
+    sf::VertexArray points {sf::PrimitiveType::Points, numPoints};
+    for (int i = 0; i < numPoints; ++i)
+    {
+        points[i].position = sf::Vector2f{mapPointToGridX(i, 0, 4500, xGridMin, xGridMax), mapPointToGridY(i, 0, 4500, yGridMin, yGridMax, windowHeight)};
+        // points[i].position = sf::Vector2f{mapPointToGridX(numPoints + i, 0, 4500, xGridMin, xGridMax), 250.f};
+        points[i].color = sf::Color::White;
     }
+
+    // std::vector<sf::VertexArray> yFixedValues {};
+
+    // float yAxisHeight { std::abs(yAxis[0].position.y - yAxis[1].position.y) };
+    // int32_t yAxisSections = static_cast<int32_t>(yAxisHeight / 10.f);
+
+    // std::cout << yAxisSections << "\n";
+
+    // float yAxisFirstPosition { yAxis[0].position.y };
+    
+    // for (size_t i {1}; i < yAxisSections - 1; ++i) {
+    //     sf::VertexArray newVertexArray {sf::PrimitiveType::Lines, 2};
+
+    //     newVertexArray[0].position = sf::Vector2f{45.f, yAxisFirstPosition + (yAxisHeight / yAxisSections) * i};
+    //     newVertexArray[1].position = sf::Vector2f{55.f, yAxisFirstPosition + (yAxisHeight / yAxisSections) * i};
+
+    //     yFixedValues.push_back(newVertexArray);
+    // }
 
     while (window.isOpen())
     {
@@ -89,15 +96,27 @@ int main()
         }
 
         window.clear();
+
         window.draw(xAxis);
         window.draw(yAxis);
         window.draw(topLimit);
-        window.draw(bottomLimit);
-        for (const auto& dash : yFixedValues)
-        {
-            window.draw(dash);
-        }
+        window.draw(rightLimit);
+
         window.draw(points);
+
+        // for (const auto& dash : yFixedValues)
+        // {
+        //     window.draw(dash);
+        // }
+
+        for (int i = 0; i < numPoints; ++i)
+        {
+            // std::cout << mapPointToGridX(1, 0, 4500, xGridMin, xGridMax) << "\n";
+            points[i].position -= sf::Vector2f(mapPointToGridX(1, 0, 4500, xGridMin, xGridMax), 0.f);
+        }
+
         window.display();
+
+        sf::sleep(sf::milliseconds(60));
     }
 }
